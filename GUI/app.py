05a5eb56017c7493aa1e126f051d6977e0b65487
Nattavee Narischat <50925464+mossZmossZ@users.noreply.github.com>
2024-10-23 05:20:32 +0700

    Update UI

--- GUI/app.py
+++ GUI/app.py
@@ -12,76 +12,99 @@ from datetime import datetime
 class Ui_BasketballCrowdCounting(object):
     def setupUi(self, BasketballCrowdCounting):
         BasketballCrowdCounting.setObjectName("BasketballCrowdCounting")
-        BasketballCrowdCounting.resize(1366, 768)
+        BasketballCrowdCounting.resize(1024, 600)
         self.centralwidget = QtWidgets.QWidget(BasketballCrowdCounting)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
-
-        # Credit Button
-        self.CreditButton = QtWidgets.QPushButton(self.centralwidget)
-        self.CreditButton.setObjectName("CreditButton")
-        self.gridLayout.addWidget(self.CreditButton, 9, 0, 1, 1)
-
-        #Open Database Button
-        self.OpenDatabase = QtWidgets.QPushButton(self.centralwidget)
-        font = QtGui.QFont()
-        font.setPointSize(26)
-        self.OpenDatabase.setFont(font)
-        self.OpenDatabase.setObjectName("OpenDatabase")
-        self.gridLayout.addWidget(self.OpenDatabase, 1, 1, 1, 1)
-
-        #Folder Label
-        self.FolderLabel = QtWidgets.QLabel(self.centralwidget)
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        self.FolderLabel.setFont(font)
-        self.FolderLabel.setObjectName("FolderLabel")
-        self.gridLayout.addWidget(self.FolderLabel, 6, 0, 1, 1)
-
-        #Total Number
-        self.Number = QtWidgets.QLabel(self.centralwidget)
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.Number.setFont(font)
-        self.Number.setObjectName("Number")
-        self.gridLayout.addWidget(self.Number, 6, 1, 1, 1)
-
-        #Table to display folder content
-        self.tableFolder = QtWidgets.QTableView(self.centralwidget)
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        self.tableFolder.setFont(font)
-        self.tableFolder.setObjectName("tableFolder")
-        self.gridLayout.addWidget(self.tableFolder, 4, 0, 1, 1)
-
-        #OpenFolderButton
+        self.CountImg = QtWidgets.QLCDNumber(self.centralwidget)
+        self.CountImg.setObjectName("CountImg")
+        self.gridLayout.addWidget(self.CountImg, 5, 1, 1, 1)
         self.OpenFolderButton = QtWidgets.QPushButton(self.centralwidget)
         font = QtGui.QFont()
         font.setPointSize(26)
         self.OpenFolderButton.setFont(font)
         self.OpenFolderButton.setObjectName("OpenFolderButton")
         self.OpenFolderButton.clicked.connect(self.openFolderDialog)  # Connect the button to openFolderDialog
-        self.gridLayout.addWidget(self.OpenFolderButton, 1, 0, 1, 1)
-
-        #Start Process Button
+        self.gridLayout.addWidget(self.OpenFolderButton, 0, 0, 1, 1)
+        self.tableFolder = QtWidgets.QTableView(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.tableFolder.setFont(font)
+        self.tableFolder.setObjectName("tableFolder")
+        self.gridLayout.addWidget(self.tableFolder, 1, 0, 2, 1)
+        self.Number = QtWidgets.QLabel(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.Number.setFont(font)
+        self.Number.setObjectName("Number")
+        self.gridLayout.addWidget(self.Number, 4, 1, 1, 1)
         self.ProcessButton = QtWidgets.QPushButton(self.centralwidget)
         font = QtGui.QFont()
         font.setPointSize(20)
         self.ProcessButton.setFont(font)
         self.ProcessButton.setObjectName("ProcessButton")
-        self.ProcessButton.setEnabled(False)  # Initially disable the Process button
         self.ProcessButton.clicked.connect(lambda: self.openNewPage(BasketballCrowdCounting))  # Pass window instance
-        self.gridLayout.addWidget(self.ProcessButton, 7, 0, 1, 1)
-
-        #Number Count
-        self.CountImg = QtWidgets.QLCDNumber(self.centralwidget)
-        self.CountImg.setObjectName("CountImg")
-        self.gridLayout.addWidget(self.CountImg, 7, 1, 1, 1)
-
+        self.gridLayout.addWidget(self.ProcessButton, 5, 0, 1, 1)
+        self.FolderLabel = QtWidgets.QLabel(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.FolderLabel.setFont(font)
+        self.FolderLabel.setObjectName("FolderLabel")
+        self.gridLayout.addWidget(self.FolderLabel, 4, 0, 1, 1)
+        self.OpenDatabase = QtWidgets.QPushButton(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(26)
+        self.OpenDatabase.setFont(font)
+        self.OpenDatabase.setObjectName("OpenDatabase")
+        self.gridLayout.addWidget(self.OpenDatabase, 0, 1, 1, 1)
+        self.CreditButton = QtWidgets.QPushButton(self.centralwidget)
+        self.CreditButton.setObjectName("CreditButton")
+        self.gridLayout.addWidget(self.CreditButton, 6, 0, 1, 1)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.YoloV5_CheckBox = QtWidgets.QCheckBox(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.YoloV5_CheckBox.setFont(font)
+        self.YoloV5_CheckBox.setAutoRepeat(False)
+        self.YoloV5_CheckBox.setObjectName("YoloV5_CheckBox")
+        self.verticalLayout.addWidget(self.YoloV5_CheckBox)
+        self.YoloV8_CheckBox = QtWidgets.QCheckBox(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.YoloV8_CheckBox.setFont(font)
+        self.YoloV8_CheckBox.setObjectName("YoloV8_CheckBox")
+        self.verticalLayout.addWidget(self.YoloV8_CheckBox)
+        self.MaskRcheckBox = QtWidgets.QCheckBox(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.MaskRcheckBox.setFont(font)
+        self.MaskRcheckBox.setObjectName("MaskRcheckBox")
+        self.verticalLayout.addWidget(self.MaskRcheckBox)
+        self.FasterRCheckbox = QtWidgets.QCheckBox(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.FasterRCheckbox.setFont(font)
+        self.FasterRCheckbox.setObjectName("FasterRCheckbox")
+        self.verticalLayout.addWidget(self.FasterRCheckbox)
+        self.CSRNetcheckBox = QtWidgets.QCheckBox(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.CSRNetcheckBox.setFont(font)
+        self.CSRNetcheckBox.setObjectName("CSRNetcheckBox")
+        self.verticalLayout.addWidget(self.CSRNetcheckBox)
+        self.gridLayout.addLayout(self.verticalLayout, 2, 1, 1, 1)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 1, 1, 1, 1)
         BasketballCrowdCounting.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(BasketballCrowdCounting)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1024, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1024, 21))
         self.menubar.setObjectName("menubar")
         BasketballCrowdCounting.setMenuBar(self.menubar)
 
@@ -90,13 +113,19 @@ class Ui_BasketballCrowdCounting(object):
 
     def retranslateUi(self, BasketballCrowdCounting):
         _translate = QtCore.QCoreApplication.translate
-        BasketballCrowdCounting.setWindowTitle(_translate("BasketballCrowdCounting", "Basketball Court Crowd Counting"))
-        self.CreditButton.setText(_translate("BasketballCrowdCounting", "Credit"))
-        self.OpenDatabase.setText(_translate("BasketballCrowdCounting", "Database"))
-        self.FolderLabel.setText(_translate("BasketballCrowdCounting", "No Folder Selected"))
-        self.Number.setText(_translate("BasketballCrowdCounting", "Total Picture"))
+        BasketballCrowdCounting.setWindowTitle(_translate("BasketballCrowdCounting", "MainWindow"))
         self.OpenFolderButton.setText(_translate("BasketballCrowdCounting", "Open Folder"))
+        self.Number.setText(_translate("BasketballCrowdCounting", "Total Image"))
         self.ProcessButton.setText(_translate("BasketballCrowdCounting", "Start Process"))
+        self.FolderLabel.setText(_translate("BasketballCrowdCounting", "No Folder Selected!"))
+        self.OpenDatabase.setText(_translate("BasketballCrowdCounting", "Open Database"))
+        self.CreditButton.setText(_translate("BasketballCrowdCounting", "Credit"))
+        self.YoloV5_CheckBox.setText(_translate("BasketballCrowdCounting", "YoloV5"))
+        self.YoloV8_CheckBox.setText(_translate("BasketballCrowdCounting", "YoloV8"))
+        self.MaskRcheckBox.setText(_translate("BasketballCrowdCounting", "Mask-R-CNN"))
+        self.FasterRCheckbox.setText(_translate("BasketballCrowdCounting", "Faster-R-CNN"))
+        self.CSRNetcheckBox.setText(_translate("BasketballCrowdCounting", "CSRNet"))
+        self.label.setText(_translate("BasketballCrowdCounting", "Select Models"))
 
     def openFolderDialog(self):
         # Open a folder dialog and get the selected folder path
@@ -121,7 +150,7 @@ class Ui_BasketballCrowdCounting(object):
         conn = sqlite3.connect('image_metadata.db')  # Connect to SQLite database
         cursor = conn.cursor()
 
-        # Create table if it doesn't exist
+        # Create table if it doesn't exist, adding the new 'model' column
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS image_metadata (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -129,7 +158,8 @@ class Ui_BasketballCrowdCounting(object):
                 time TEXT,
                 people_count INTEGER DEFAULT NULL,
                 image_path TEXT,
-                predict_path TEXT DEFAULT NULL
+                predict_path TEXT DEFAULT NULL,
+                model TEXT DEFAULT NULL  -- New column 'model'
             )
         ''')
 
@@ -140,51 +170,41 @@ class Ui_BasketballCrowdCounting(object):
             if filename.lower().endswith(('.jpg', '.jpeg', '.png')):  # Filter image files
                 image_path = os.path.join(folder_path, filename)
                 image_count += 1
-
                 try:
                     # Open the image
                     image = Image.open(image_path)
-
                     # Extracting the exif metadata
                     exifdata = image.getexif()
-
                     # Initialize variables for date and time
                     date = None
                     time = None
-
                     # Loop through all the tags present in exifdata
                     for tagid in exifdata:
                         tagname = TAGS.get(tagid, tagid)
                         value = exifdata.get(tagid)
-
                         # Looking for the DateTime tag
                         if tagname == 'DateTime':
                             # Original format: 'YYYY:MM:DD HH:MM:SS'
                             datetime_str = value
-                            
                             # Converting to datetime object
                             dt_obj = datetime.strptime(datetime_str, '%Y:%m:%d %H:%M:%S')
-                            
                             # Formatting the date as YYYY-MM-DD
                             date = dt_obj.strftime('%Y-%m-%d')
-                            
                             # Formatting the time as HH:MM
                             time = dt_obj.strftime('%H:%M')
-
-                    # Insert the extracted data into the database, setting people_count and predict_path as None
+                    # Insert the extracted data into the database, setting people_count, predict_path, and model as None
                     cursor.execute('''
-                        INSERT INTO image_metadata (date, time, people_count, image_path, predict_path)
-                        VALUES (?, ?, ?, ?, ?)
-                    ''', (date, time, None, image_path, None))
-
+                        INSERT INTO image_metadata (date, time, people_count, image_path, predict_path, model)
+                        VALUES (?, ?, ?, ?, ?, ?)
+                    ''', (date, time, None, image_path, None, None))  # 'model' is set to None
                     print(f"Processed: {image_path} - Date: {date}, Time: {time}")
-
                 except Exception as e:
                     print(f"Failed to process {filename}: {e}")
-
+        
         # Commit the transaction and close the connection
         conn.commit()
         conn.close()
+
 
         # Update the LCD display and label after processing
         self.CountImg.display(image_count)
